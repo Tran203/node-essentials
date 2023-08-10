f7ad384b855183740c74d5fc12c918129ac413b1
Tran203 <tshetshe203@gmail.com>
2023-08-10 10:01:24 +0200

    update

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -7,21 +7,34 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.post('/products', function (req, res) {
-  // implement
-});
-
-app.put('/products', function (req, res) {
-  // implement
-});
-
-app.delete('/products/:id', function (req, res) {
-  // implement
-});
-
-app.get('/products', (req, res) => {
-  // implement
-})
+app.route('/products')
+  //return products
+  .get((req, res) => {
+    res.json(products);
+  })
+  //post/create products
+  .post((req, res) => {
+    const newProduct = { ...req.body, id: products.length + 1 }
+    products = [...products, newProduct]
+    res.json(newProduct);
+  })
+  //ability to update
+  .put((req, res) => {
+    let updatedProduct;
+    products = products.map(p => {
+      if (p.id === req.body.id) {
+        updatedProduct = { ...p, ...req.body };
+        return updatedProduct;
+      }
+      return p;
+    })
+    res.json(updatedProduct);
+  })
+  //delete
+  .delete((req, res) => {
+    const deletedProduct = products.find(p => p.id === +req.body.id);
+    products = products.filter(p => p.id !== +req.body.id);
+    res.json(deletedProduct);
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-  
